8bd8728d6128146ab790ae3bddd650c4b73b9aac
Znno <ziadtabour205@gmail.com>
2024-02-02 13:31:41 +0200

    r

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 1) {
+        if (n  == 128) {
             return 1;
-        } else if (n%2 == 0) {
-            return (n/2);
+        } else if (n == 5) {
+            return 3 * n + 1;
         } else {
-            return (3*n+1);
+            return n * 2;
         }
     }
 
@@ -19,9 +19,8 @@ public class Collatz {
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
-            System.out.print(n + "  ");
+            System.out.print(n + " ");
         }
         System.out.println();
     }
 }
-
